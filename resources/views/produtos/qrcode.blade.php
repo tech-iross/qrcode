9eb987048314078721423ec67b1c021eadd090dd
italo.silva <italo.computation@gmail.com>
2025-11-20 14:42:24 -0400

    fix/novo-ajuste

--- resources/views/produtos/qrcode.blade.php
+++ resources/views/produtos/qrcode.blade.php
@@ -1,5 +1,7 @@
-<h3>QR Code do Produto {{ $produto->codigo }}</h3>
+<h3 style="text-align: center; margin-bottom: 20px;">
+    QR Code do Produto {{ $produto->codigo }}
+</h3>
 
-<div>
-    {!! \SimpleSoftwareIO\QrCode\Facades\QrCode::size(90)->generate($produto->codigo) !!}
+<div style="display: flex; justify-content: center; align-items: center; margin-top: 20px;">
+    {!! \SimpleSoftwareIO\QrCode\Facades\QrCode::size(250)->margin(2)->generate($produto->codigo) !!}
 </div>
